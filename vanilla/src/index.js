6e69546cb598c797a477f4af672577222e97da41
Karl Hofer <kh@viewar.com>
2019-01-03 09:49:05 +0100

    [VASB-6380] Update webpack config for vanilla

--- vanilla/src/index.js
+++ vanilla/src/index.js
@@ -1,15 +1,23 @@
-import viewarApi from 'viewar-api'
+import viewarApi from 'viewar-api';
 
-import './index.css'
+import './index.scss';
 
-;(async function main () {
-  window.api = await viewarApi.init()
+(async function main() {
+  window.api = await viewarApi.init();
 
-  const sheepModel = await viewarApi.modelManager.fetchModelFromRepository('20')
+  const sheepModel = await viewarApi.modelManager.fetchModelFromRepository(
+    '20'
+  );
 
   for (let x = 0; x < 20; ++x) {
     await viewarApi.sceneManager.insertModel(sheepModel, {
-      pose: {position: {x: (Math.random() * 4000 - 2000), y: 0, z: (Math.random() * 4000 - 2000)}}
-    })
+      pose: {
+        position: {
+          x: Math.random() * 4000 - 2000,
+          y: 0,
+          z: Math.random() * 4000 - 2000,
+        },
+      },
+    });
   }
-}())
+})();
